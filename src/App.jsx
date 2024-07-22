eca8269ffb351a027e9a6ce3dd21ffa5fbf89872
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-07-22 10:25:30 +0500

    project change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,78 @@
+import { useState } from "react";
 import React from "react";
 import styles from "./App.module.css";
 
 function App() {
+  const [form, setFrom] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [country, setCountry] = useState("O'zbekiston");
+  const [city, setCity] = useState("Toshkent");
+  const [address, setAddress] = useState("");
+  const [count, setCount] = useState(null);
+  const [desc, setDesc] = useState("");
+
+  function handleName(event) {
+    setName(event.target.value);
+  }
+
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePhone(event) {
+    setPhone(event.target.value);
+  }
+
+  function handleCountry(event) {
+    setCountry(event.target.value);
+  }
+
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+
+  function handleAddress(event) {
+    setAddress(event.target.value);
+  }
+
+  function handleCount(event) {
+    setCount(event.target.value);
+  }
+
+  function handleDesc(event) {
+    setDesc(event.target.value);
+  }
+
+  function Setlocal() {
+    let users = [];
+    if (localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"));
+    }
+    return users;
+  }
+
+  function handleForm(event) {
+    event.preventDefault();
+    let user = {
+      name:name,
+      email:email,
+      phone:phone,
+      country:country,
+      city:city,
+      address:address,
+      count:count,
+      description:desc
+    }
+
+    let users = Setlocal()
+    users.push(user)
+    localStorage.setItem('users', JSON.stringify(users)) 
+  }
+
   return (
-    <div className={styles.container1}>
+    <div>
       <header className={styles.info}>
         <div className={styles.nav}>
           <img
@@ -50,26 +119,45 @@ function App() {
               </div>
 
               <div className={styles.inputs}>
-                <p className={styles.teg}>Kompaniya nomi *</p>
+                <p className={styles.teg}>
+                  Kompaniya nomi <h>*</h>
+                </p>
                 <input
                   type="text"
                   id={styles.name}
                   placeholder="Kompaniya nomi"
+                  onChange={handleName}
                 />
               </div>
 
               <div className={styles.inputs}>
-                <p className={styles.teg}>Email *</p>
-                <input type="text" id={styles.email} placeholder="Email" />
+                <p className={styles.teg}>
+                  Email <h>*</h>
+                </p>
+                <input
+                  type="text"
+                  id={styles.email}
+                  placeholder="Email"
+                  onChange={handleEmail}
+                />
               </div>
 
               <div className={styles.inputs}>
-                <p className={styles.teg}>Telefon raqami *</p>
-                <input type="number" id={styles.phone} placeholder="UZ +998" />
+                <p className={styles.teg}>
+                  Telefon raqami <h>*</h>
+                </p>
+                <input
+                  type="number"
+                  id={styles.phone}
+                  placeholder="UZ +998"
+                  onChange={handlePhone}
+                />
               </div>
 
               <div className={styles.social}>
-                <p>Linklar *</p>
+                <p>
+                  Linklar <h>*</h>
+                </p>
                 <span>
                   <button>
                     <a href="#">
@@ -126,8 +214,10 @@ function App() {
 
               <div className={styles.from}>
                 <div className={styles.country}>
-                  <p>Davlat *</p>
-                  <select name="" id={styles.Davlat}>
+                  <p>
+                    Davlat <h>*</h>
+                  </p>
+                  <select name="" id={styles.Davlat} onChange={handleCountry}>
                     <option value="O'zbekiston">O'zbekiston</option>
                     <option value="Rossiya">Rossiya</option>
                     <option value="USA">USA</option>
@@ -135,8 +225,10 @@ function App() {
                 </div>
 
                 <div className={styles.country}>
-                  <p>Shahar *</p>
-                  <select name="" id={styles.Shahar}>
+                  <p>
+                    Shahar <h>*</h>
+                  </p>
+                  <select name="" id={styles.Shahar} onChange={handleCity}>
                     <option value="Toshkent">Toshkent</option>
                     <option value="Qo'qon">Qo'qon</option>
                     <option value="Nukus">Nukus</option>
@@ -145,22 +237,34 @@ function App() {
               </div>
 
               <div className={styles.inputs0}>
-                <p className={styles.teg}>Yashash joyi *</p>
-                <input type="text" id={styles.address} placeholder="Joy" />
+                <p className={styles.teg}>
+                  Yashash joyi <h>*</h>
+                </p>
+                <input
+                  type="text"
+                  id={styles.address}
+                  placeholder="Joy"
+                  onChange={handleAddress}
+                />
               </div>
               <div className={styles.inputs}>
-                <p className={styles.teg}>Hodimlar soni *</p>
+                <p className={styles.teg}>
+                  Hodimlar soni <h>*</h>
+                </p>
                 <input
                   type="number"
                   id={styles.staff}
                   placeholder="Hodimlar soni"
+                  onChange={handleCount}
                 />
               </div>
 
               <div className={styles.text}>
-                <p>Izoh *</p>
-                <textarea name="text" id={styles.comment}>
-                  Kompaniya haqida izoh qoldiring
+                <p>
+                  Izoh <h>*</h>
+                </p>
+                <textarea name="text" id={styles.comment} placeholder="Kompaniya haqida izoh qoldiring"g onChange={handleDesc}>
+                  
                 </textarea>
               </div>
             </div>
@@ -168,7 +272,7 @@ function App() {
 
           <div className={styles.button}>
             <button className={styles.first}>ORTGA</button>
-            <button className={styles.second}>KEYINGISI</button>
+            <button className={styles.second} onClick={handleForm}>KEYINGISI</button>
           </div>
         </form>
       </section>
